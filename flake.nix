d29f08b7590222c6a580ffbac78d79d32311373a
Ian Johannesen <ij@opsplaza.com>
2026-01-26 19:16:46 +0100

    chore: update pakhet to nixos-25.05 stable

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     };
 
     nixpkgs-stable = {
-      url = "github:NixOS/nixpkgs/nixos-24.11";
+      url = "github:NixOS/nixpkgs/nixos-25.05";
     };
 
     home-manager = {
@@ -16,7 +16,7 @@
     };
 
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
