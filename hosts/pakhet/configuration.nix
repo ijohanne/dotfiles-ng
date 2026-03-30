e5bd90f76990873198d4e6f612b5b0ff0dd1971c
Ian Johannesen <ij@opsplaza.com>
2026-03-30 20:07:23 +0200

    pakhet: pin ipv6 and disable rogue ra

--- hosts/pakhet/configuration.nix
+++ hosts/pakhet/configuration.nix
@@ -22,6 +22,28 @@ in
     hostName = "pakhet";
     useDHCP = true;
     nameservers = [ "${network.hosts.goose.ips.wired}" ];
+    interfaces.enp0s5.ipv6.addresses = [
+      {
+        address = network.hosts.pakhet.ip6;
+        prefixLength = 64;
+      }
+    ];
+    defaultGateway6 = {
+      address = network.hosts.goose.ip6s.wired;
+      interface = "enp0s5";
+    };
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv6.conf.all.accept_ra" = lib.mkForce 0;
+    "net.ipv6.conf.default.accept_ra" = lib.mkForce 0;
+    "net.ipv6.conf.enp0s5.accept_ra" = lib.mkForce 0;
+    "net.ipv6.conf.all.autoconf" = lib.mkForce 0;
+    "net.ipv6.conf.default.autoconf" = lib.mkForce 0;
+    "net.ipv6.conf.enp0s5.autoconf" = lib.mkForce 0;
+    "net.ipv6.conf.all.use_tempaddr" = lib.mkForce 0;
+    "net.ipv6.conf.default.use_tempaddr" = lib.mkForce 0;
+    "net.ipv6.conf.enp0s5.use_tempaddr" = lib.mkForce 0;
   };
 
   # kresd is pulled in by nixos-mailserver for DANE — keep it for postfix
