74650f8798d1d6e02cb4536f19450fa517c1cc61
Ian Johannesen <ij@opsplaza.com>
2026-04-02 21:17:13 +0200

    feat(goose): enable geo-offset tracking, expand GPS Quality row

--- hosts/goose/services/monitoring/gpsd.nix
+++ hosts/goose/services/monitoring/gpsd.nix
@@ -5,5 +5,8 @@
     enableLocalScraping = true;
     gpsdHost = network.hosts.chronos-wired.ip;
     ppsHistogram = true;
+    offsetFromGeopoint = true;
+    geopointLat = 36.4240;
+    geopointLon = -5.1524;
   };
 }
