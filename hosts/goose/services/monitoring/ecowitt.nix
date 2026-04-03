e071fe5260d442be2768c6c4e954281244606e2a
Ian Johannesen <ij@opsplaza.com>
2026-04-03 15:14:53 +0200

    feature(ecowitt): change from wm2 to klx unit

--- hosts/goose/services/monitoring/ecowitt.nix
+++ hosts/goose/services/monitoring/ecowitt.nix
@@ -3,6 +3,7 @@
 {
   services.prometheus-ecowitt-exporter = {
     enable = true;
+    irradianceUnit = "klx";
     enableLocalScraping = true;
     enableGrafanaDashboard = true;
     forwardUrls = [ "http://10.255.240.3:8080/data/report/" ];
