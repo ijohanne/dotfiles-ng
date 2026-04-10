00344ad5e623bbf86d23ff276c55a4fe8e447087
Ian Johannesen <ij@opsplaza.com>
2026-04-10 14:38:53 +0200

    feat(goose): add remote wireguard peers

--- modules/private/nixos/services/goose/wireguard.nix
+++ modules/private/nixos/services/goose/wireguard.nix
@@ -47,6 +47,16 @@
           publicKey = "qlI2m68iQCaoGIaa7wU41IyTTWg0fsRjkD4OmWcYzFc=";
           allowedIPs = [ "${network.hosts.wg-anubis.ip}/32" ];
         }
+        {
+          # ij-remote
+          publicKey = "vI7obi1Ekyt49W0il13kpckOaNjR4ryBCDevzcsv1hM=";
+          allowedIPs = [ "${network.hosts.wg-ij-remote.ip}/32" ];
+        }
+        {
+          # mt-remote
+          publicKey = "SyWatfaLXku+dgVSvqKBGGrrFx+V9ef1e6Pv/1yNb0Y=";
+          allowedIPs = [ "${network.hosts.wg-mt-remote.ip}/32" ];
+        }
       ];
     };
   };
