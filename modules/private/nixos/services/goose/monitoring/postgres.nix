db55805156d39ce8b63f72039b92d984f57043b9
Ian Johannesen <ij@opsplaza.com>
2026-04-25 00:15:55 +0200

    feat(goose): scrape ops postgresql exporters

--- modules/private/nixos/services/goose/monitoring/postgres.nix
+++ modules/private/nixos/services/goose/monitoring/postgres.nix
@@ -8,7 +8,21 @@
       static_configs = [
         {
           targets = [ "${network.hosts.pakhet.ip}:9630" ];
-          labels = { instance = "pakhet"; };
+          labels = {
+            instance = "pakhet";
+          };
+        }
+        {
+          targets = [ "${network.hosts.wg-app-srv-00-rbx-fr.ip}:9630" ];
+          labels = {
+            instance = "app-srv-00.rbx.fr";
+          };
+        }
+        {
+          targets = [ "${network.hosts.wg-app-srv-00-nur-de.ip}:9630" ];
+          labels = {
+            instance = "app-srv-00.nur.de";
+          };
         }
       ];
     }
