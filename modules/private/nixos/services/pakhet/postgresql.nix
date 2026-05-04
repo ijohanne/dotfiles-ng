2cb66a6e2410f71f661687742fba6ff12403f235
Ian Johannesen <ij@opsplaza.com>
2026-05-04 10:01:17 +0200

    fix(pakhet): raise postgres connection limit

--- modules/private/nixos/services/pakhet/postgresql.nix
+++ modules/private/nixos/services/pakhet/postgresql.nix
@@ -5,5 +5,9 @@
     enable = true;
     package = pkgs.postgresql_18;
     extensions = ps: [ ps.postgis ];
+    settings = {
+      max_connections = 200;
+      superuser_reserved_connections = 10;
+    };
   };
 }
